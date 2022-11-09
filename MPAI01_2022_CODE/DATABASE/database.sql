ad5a85fc9729d14a721cc6d207c947af108d91fb
Yuvrajgnitc <117837240+Yuvrajgnitc@users.noreply.github.com>
2022-11-09 20:00:35 +0530

    Add files via upload

---
+++ MPAI01_2022_CODE/DATABASE/database.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Community v8.71 
+MySQL - 5.5.30 : Database - vtpai08
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`vtpai08` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `vtpai08`;
+
+/*Table structure for table `feedbacks` */
+
+DROP TABLE IF EXISTS `feedbacks`;
+
+CREATE TABLE `feedbacks` (
+  `name` varchar(255) DEFAULT NULL,
+  `item` varchar(255) DEFAULT NULL,
+  `contact_no` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `feedback` varchar(255) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `feedbacks` */
+
+insert  into `feedbacks`(`name`,`item`,`contact_no`,`email`,`feedback`,`rating`) values ('lavan','chicken biryani','8499839022','lavan@gmail.com','good',2),('seekanth','chicken biryani','4566325655','sreeforall@gmail.com','good',4);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
